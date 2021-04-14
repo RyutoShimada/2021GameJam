4348ebba32329682545023e298f2d0373777bfa7
RyutoShimada <business.sub10110@gmail.com>
2021-04-14 15:41:24 +0900

    ボムの生成のしかたをレベルごとに変えられるようにした ShimadaManagerからGeneratorを操作するようにした

--- Assets/ShimdaScripts/BomGenerator.cs
+++ Assets/ShimdaScripts/BomGenerator.cs
@@ -5,36 +5,35 @@ using UnityEngine;
 public class BomGenerator : MonoBehaviour
 {
     [SerializeField] GameObject[] m_boms = null;
-    [SerializeField] float m_interval = 1f;
-    [SerializeField] int m_addTimeInterval = 10;
-    [SerializeField] float m_subtraction = 0.1f;
+    public float m_interval = 3f;
+    public int m_count = 1;
     float m_time = 0;
     int m_index = 0;
-    float m_speedUpTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_time = m_interval;
     }
 
     // Update is called once per frame
     void Update()
     {
         m_time += Time.deltaTime;
-        m_speedUpTime += Time.deltaTime;
+        Generate(m_interval, m_count, m_time);
+    }
 
-        if (m_speedUpTime > m_addTimeInterval)//生成する間隔を短くするインターバル
-        {
-            m_speedUpTime = 0;
-            m_interval -= m_subtraction;//生成する間隔を短くする
-        }
 
-        if (m_time > m_interval)
+    void Generate(float interval, int count, float time)
+    {
+        if (time > interval)
         {
             m_time = 0;
-            m_index = Random.Range(0, m_boms.Length);
-            Instantiate(m_boms[m_index], transform.position, transform.rotation);
+            for (int i = 0; i < count; i++)
+            {
+                m_index = Random.Range(0, m_boms.Length);
+                Instantiate(m_boms[m_index], transform.position, transform.rotation);
+            }
         }
     }
 }
