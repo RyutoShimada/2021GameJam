b916999ea85f0bf698347e35b35d5c688d9a6850
RyutoShimada <business.sub10110@gmail.com>
2021-04-14 11:37:10 +0900

    生成する間隔を調整できるようにした

--- Assets/ShimdaScripts/BomGenerator.cs
+++ Assets/ShimdaScripts/BomGenerator.cs
@@ -6,8 +6,11 @@ public class BomGenerator : MonoBehaviour
 {
     [SerializeField] GameObject[] m_boms = null;
     [SerializeField] float m_interval = 1f;
+    [SerializeField] int m_addTimeInterval = 10;
+    [SerializeField] float m_subtraction = 0.1f;
     float m_time = 0;
     int m_index = 0;
+    float m_speedUpTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,13 @@ public class BomGenerator : MonoBehaviour
     void Update()
     {
         m_time += Time.deltaTime;
+        m_speedUpTime += Time.deltaTime;
+
+        if (m_speedUpTime > m_addTimeInterval)//生成する間隔を短くするインターバル
+        {
+            m_speedUpTime = 0;
+            m_interval -= m_subtraction;//生成する間隔を短くする
+        }
 
         if (m_time > m_interval)
         {
