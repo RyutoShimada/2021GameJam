f24b7a4f2f9305131dc338a125759b2cd017158d
D-Itsuki <raito.kaminari@gmail.com>
2021-04-14 10:50:40 +0900

    エリアプロトタイプ追加

---
+++ Assets/DaitoScripts/AreaController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaController : MonoBehaviour
+{
+    [SerializeField] string thisColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+        if (collision.tag != thisColor)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
