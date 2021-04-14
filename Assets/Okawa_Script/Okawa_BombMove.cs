da4db4bc707cf02c634e5a20c945360e6b3178a8
rutu009 <8901diowryyy@gmail.com>
2021-04-14 11:44:50 +0900

    ボム兵が動くようにしました

---
+++ Assets/Okawa_Script/Okawa_BombMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Okawa_BombMove : MonoBehaviour
+{
+    [SerializeField] float m_pushPowerx;
+    [SerializeField] float m_pushPowery;
+    Rigidbody2D m_rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rb2d.velocity = new Vector2(m_pushPowerx, m_pushPowery);
+    }
+}
